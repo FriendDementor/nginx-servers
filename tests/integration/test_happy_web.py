03d89b8c5802c88f09f24f529fee08d5f91cef74
Pablo Vannini <vanninipablo@gmail.com>
2021-03-01 03:03:33 -0300

    adding checks before add and enable commands

--- tests/integration/test_happy_web.py
+++ tests/integration/test_happy_web.py
@@ -3,6 +3,7 @@ import unittest
 import shutil
 import os
 import re
+import time
 
 def execute(command):
     result = subprocess.check_output(command.split())
@@ -20,6 +21,8 @@ def clean_files():
             for d in dirs:
                 shutil.rmtree(os.path.join(root, d))
 
+    execute("nginx -s reload")
+
 class TestFunctionsHappyPath(unittest.TestCase):
 
     def tearDown(self):
@@ -32,23 +35,26 @@ class TestFunctionsHappyPath(unittest.TestCase):
         clean_files()
 
     def test_nsc_simple_add_web(self):
-
-        result = execute("nsc add example.com")
-        self.assertEqual(result, "OK\n")
-
-        result = execute("nsc enable example.com")
-        self.assertEqual(result, "OK\n")
-
+        # By default the curl should show a 404
         result = subprocess.run(['curl', '-v', 'http://example.com/'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         result = result.stdout.decode("utf-8")
+        self.assertTrue("< HTTP/1.1 404 Not Found" in result)
 
+        # After simply add a templated domain, curl should still show 404
+        result = execute("nsc add example.com")
+        self.assertEqual(result, "OK\n")
+        result = subprocess.run(['curl', '-v', 'http://example.com/'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        result = result.stdout.decode("utf-8")
+        self.assertTrue("< HTTP/1.1 404 Not Found" in result)
+
+        # Now when enable the domain, curl should works fine
+        result = execute("nsc enable example.com")
+        time.sleep(0.2) # wait a bit time for give time to nginx server to update
+        self.assertEqual(result, "OK\n")
+        result = subprocess.run(['curl', '-v', 'http://example.com/'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        result = result.stdout.decode("utf-8")
         self.assertTrue("< HTTP/1.1 200 OK" in result)
         self.assertTrue("< Content-Type: text/html" in result)
         self.assertTrue("<title>example.com</title>" in result)
         self.assertTrue("The content of example.com" in result)
-
-
-
-
-
 
